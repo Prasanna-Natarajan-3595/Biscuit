929774bec76a944d0275a33347f5e51743abc82e
Prasanna <prasannanatarajan3595@gmail.com>
2021-04-05 18:11:49 +0530

    Delete view.py

--- web/view.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request, flash,redirect,url_for,session
-
-view = Blueprint('view',__name__)
-
-@view.route('/app')
-def app():
-    return render_template('app.html')
